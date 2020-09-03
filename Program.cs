792e5e5bd55aeb05d26b290b381c254e56895fa0
KaterinPerdom <katerinperdom9001@gmail.com>
2020-09-03 15:52:13 -0500

    Se deserializo los objetos de Json en busqueda de obtener solo algunos atributos para luego imprimirlos por consola

--- Program.cs
+++ Program.cs
@@ -11,7 +11,12 @@ namespace MyFirtProjectC_
             string API_URL ="https://jsonplaceholder.typicode.com/posts?_limit=5";
             var client = new WebClient();
             var json = client.DownloadString(API_URL);
-            Console.WriteLine(json);
+            dynamic posts = JsonConvert.DeserializeObject(json);
+            
+            foreach(var post in posts){
+                Console.WriteLine(post.id + ": "+ post.title);
+            }
+
         }
     }
 }
