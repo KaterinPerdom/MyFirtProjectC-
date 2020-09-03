de6fd5f50715205b0fb2afed84d51352276a924d
KaterinPerdom <katerinperdom9001@gmail.com>
2020-09-03 15:40:15 -0500

    Conexion con jsonplaceholder para obtener unos posts
    Se prueba por consola para observar que esta trayendo desde esta paginos unos post determinados

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MyFirtProjectC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL ="https://jsonplaceholder.typicode.com/posts?_limit=5";
+            var client = new WebClient();
+            var json = client.DownloadString(API_URL);
+            Console.WriteLine(json);
+        }
+    }
+}
